408e2441aefafdc1c2a9f61f6f6ff20f4c14db6b
Denis R <denis_15_abril@hotmail.com>
2023-02-12 11:33:30 -0500

    size

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ switchModeButton.addEventListener("click", () => {
   body.classList.toggle("dark-mode");
   body.classList.toggle("light-mode");
 });
+
+const button = document.getElementById("cv");
+button.addEventListener("click", function() {
+  const link = document.createElement("a");
+  link.href = "https://drive.google.com/file/d/1SjcIpvsXlRKcjbKtai2ay2tJmyJxC1Ax/view?usp=sharing";
+  link.target = "_blank";
+  link.click();
+});
